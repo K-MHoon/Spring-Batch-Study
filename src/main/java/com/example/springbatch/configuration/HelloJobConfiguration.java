3c8a8d7280f9702ffae502940a8f14c212e29061
K-MHoon <azmria117@gmail.com>
2022-02-04 15:08:02 +0900

    feat : FlowJobConfiguration 구현 (Flow 방식 테스트)
    - Controller 임시 중단
    - 비동기 중단

--- src/main/java/com/example/springbatch/configuration/HelloJobConfiguration.java
+++ src/main/java/com/example/springbatch/configuration/HelloJobConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 @RequiredArgsConstructor
 public class HelloJobConfiguration {
--- src/main/java/com/example/springbatch/controller/JobLauncherController.java
+++ src/main/java/com/example/springbatch/controller/JobLauncherController.java
@@ -23,9 +23,10 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class JobLauncherController {
 
-    private final Job job;
-    private final JobLauncher jobLauncher;
-    private final BasicBatchConfigurer basicBatchConfigurer;
+    // TODO JobLauncher 충돌로 임시 컨트롤러 중단
+//    private final Job job;
+//    private final JobLauncher jobLauncher;
+//    private final BasicBatchConfigurer basicBatchConfigurer;
 
     @PostMapping("/batch")
     public String launch(@RequestBody Member member) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
@@ -36,10 +37,14 @@ public class JobLauncherController {
 
         // JobLauncher 비동기 실행
         // SimpleJobLauncher(proxy) 는 직접 DI로 받을 수 없음. BasicBatchConfigurer가 실제 객체를 가지고 있음.
+        /*
+
         SimpleJobLauncher jobLauncher = (SimpleJobLauncher) basicBatchConfigurer.getJobLauncher();
         jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
 
         jobLauncher.run(job, jobParameters);
+
+         */
 
         return "batch completed";
     }
