51e417d665eaf1cfbe1d8cfecab18df02ae2ddee
K-MHoon <azmria117@gmail.com>
2022-02-15 21:32:43 +0900

    feat : JdbcCursorItemReader 활용하여 Cursor 기반 DB 데이터 읽는 방식 구현

---
+++ src/main/java/com/example/springbatch/configuration/JdbcCursorConfiguration.java
@@ -0,0 +1,66 @@
+package com.example.springbatch.configuration;
+
+import com.example.springbatch.dto.Customer4;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class JdbcCursorConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private int chunkSize = 10;
+    private final DataSource dataSource;
+
+    @Bean
+    public Job jdbcCursorJob() {
+        return jobBuilderFactory.get("jdbcCursorJob")
+                .start(jdbcCursorStep1())
+                .build();
+    }
+
+    @Bean
+    public Step jdbcCursorStep1() {
+        return stepBuilderFactory.get("jdbcCursorStep1")
+                .<Customer4, Customer4>chunk(chunkSize)
+                .reader(jdbcCursorItemReader())
+                .writer(jdbcCursorItemWriter())
+                .build();
+    }
+
+    @Bean
+    public ItemReader<Customer4> jdbcCursorItemReader() {
+
+        return new JdbcCursorItemReaderBuilder<Customer4>()
+                .name("jdbcCursorItemReader")
+                .fetchSize(chunkSize)
+                .sql("select id, firstName, lastName, birthdate from customer where firstName like ? order by lastName, firstName")
+                .beanRowMapper(Customer4.class)
+                .queryArguments("A%")
+                .dataSource(dataSource)
+                .build();
+    }
+
+    @Bean
+    public ItemWriter<? super Customer4> jdbcCursorItemWriter() {
+        return items -> {
+            for (Customer4 item : items) {
+                System.out.println("item = " + item.toString());
+            }
+        };
+    }
+
+
+}
---
+++ src/main/java/com/example/springbatch/dto/Customer4.java
@@ -0,0 +1,12 @@
+package com.example.springbatch.dto;
+
+import lombok.Data;
+
+@Data
+public class Customer4 {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String birthdate;
+}
