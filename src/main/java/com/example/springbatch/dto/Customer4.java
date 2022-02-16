93cf3996a8eb4946c38bc17546e359113bee6c88
K-MHoon <azmria117@gmail.com>
2022-02-16 21:59:00 +0900

    feat : JpaCursorItemReader 활용하여 EntityManager + Cursor 기반 DB 데이터 읽는 방식 구현

--- src/main/java/com/example/springbatch/dto/Customer4.java
+++ src/main/java/com/example/springbatch/dto/Customer4.java
@@ -2,10 +2,16 @@ package com.example.springbatch.dto;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "customer")
 public class Customer4 {
 
+    @Id @GeneratedValue
     private Long id;
+
     private String firstName;
     private String lastName;
     private String birthdate;
