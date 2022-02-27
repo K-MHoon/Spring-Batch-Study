32b663a49779f5353e8cc74fc55af10c5b31d1b9
K-MHoon <azmria117@gmail.com>
2022-02-27 14:56:14 +0900

    feat : ChunkListener, Item Read,Write,Process Listener 구현

---
+++ src/main/java/com/example/springbatch/listener/CustomItemReadListener.java
@@ -0,0 +1,20 @@
+package com.example.springbatch.listener;
+
+import org.springframework.batch.core.ItemReadListener;
+
+public class CustomItemReadListener implements ItemReadListener {
+    @Override
+    public void beforeRead() {
+        System.out.println(">> before Read");
+    }
+
+    @Override
+    public void afterRead(Object item) {
+        System.out.println(">> after Read");
+    }
+
+    @Override
+    public void onReadError(Exception ex) {
+        System.out.println(">> read Error");
+    }
+}
