ff9d9ea0b67366c36b3af413f08315abd738fa82
K-MHOON <azmria117@gmail.com>
2022-03-12 08:54:07 +0900

    feat : [계좌 예제] 계좌 요약 엔티티 Step, Item Reader, Processor, Writer 구현

--- study/src/main/java/com/example/study/configuration/TransactionConfiguration.java
+++ study/src/main/java/com/example/study/configuration/TransactionConfiguration.java
@@ -1,20 +1,21 @@
 package com.example.study.configuration;
 
+import com.example.study.entity.AccountSummary;
 import com.example.study.entity.Transaction;
+import com.example.study.processor.TransactionItemProcessor;
 import com.example.study.reader.TransactionItemReader;
+import com.example.study.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
-import org.quartz.JobBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.item.ItemStreamReader;
-import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
-import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.PassThroughFieldSetMapper;
@@ -34,6 +35,7 @@ public class TransactionConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final DataSource dataSource;
+    private final TransactionRepository transactionRepository;
 
     @Bean
     @StepScope
@@ -53,6 +55,7 @@ public class TransactionConfiguration {
     }
 
     @Bean
+    @StepScope
     public JdbcBatchItemWriter<Transaction> transactionItemWriter() {
         return new JdbcBatchItemWriterBuilder<Transaction>()
                 .dataSource(dataSource)
@@ -66,6 +69,7 @@ public class TransactionConfiguration {
     }
 
     @Bean
+    @JobScope
     public Step importTransactionFileStep() {
         return stepBuilderFactory.get("importTransactionFileStep")
                 .<Transaction, Transaction>chunk(100)
@@ -73,6 +77,45 @@ public class TransactionConfiguration {
                 .writer(transactionItemWriter())
                 .allowStartIfComplete(true)
                 .listener(transactionItemReader())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public JdbcCursorItemReader<AccountSummary> accountSummaryItemReader() {
+        return new JdbcCursorItemReaderBuilder<AccountSummary>()
+                .name("accountSummaryItemReader")
+                .dataSource(dataSource)
+                .beanRowMapper(AccountSummary.class)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public TransactionItemProcessor transactionItemProcessor() {
+        return new TransactionItemProcessor(transactionRepository);
+    }
+
+    @Bean
+    @StepScope
+    public JdbcBatchItemWriter<AccountSummary> accountSummaryWriter() {
+        return new JdbcBatchItemWriterBuilder<AccountSummary>()
+                .dataSource(dataSource)
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("update tbl_account_summary " +
+                        "set current_balance = :currentBalance " +
+                        "where account_number = :accountNumber")
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step applyTransactionStep() {
+        return stepBuilderFactory.get("applyTransactionStep")
+                .<AccountSummary, AccountSummary>chunk(100)
+                .reader(accountSummaryItemReader())
+                .processor(transactionItemProcessor())
+                .writer(accountSummaryWriter())
                 .build();
     }
 }
