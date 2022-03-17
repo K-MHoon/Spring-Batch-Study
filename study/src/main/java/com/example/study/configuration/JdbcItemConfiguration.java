5e5075b6c5fc33ac210d052fa1ae6f3236c4ae8c
K-MHOON <azmria117@gmail.com>
2022-03-17 20:25:59 +0900

    feat : JdbcPagingItemReader 활용하여 Jdbc 페이징 처리기법 구현
    - PagingQueryProvider의 구현체인 SqlPagingQueryProviderFactoryBean을 활용하여 자동 데이터베이스 종류 매핑

--- study/src/main/java/com/example/study/configuration/JdbcItemConfiguration.java
+++ study/src/main/java/com/example/study/configuration/JdbcItemConfiguration.java
@@ -1,6 +1,5 @@
 package com.example.study.configuration;
 
-import com.example.study.dto.MyCustomer3;
 import com.example.study.dto.MyCustomer4;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
@@ -11,13 +10,19 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.PagingQueryProvider;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.ArgumentPreparedStatementSetter;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -47,7 +52,7 @@ public class JdbcItemConfiguration {
     }
 
     @Bean
-    public Job jdbcItemCursorJob() {
+    public Job jdbcItemCursorJob() throws Exception {
         return jobBuilderFactory.get("jdbcItemCursorJob")
                 .incrementer(new RunIdIncrementer())
                 .start(jdbcItemCursorStep())
@@ -55,10 +60,11 @@ public class JdbcItemConfiguration {
     }
 
     @Bean
-    public Step jdbcItemCursorStep() {
+    public Step jdbcItemCursorStep() throws Exception {
         return stepBuilderFactory.get("jdbcItemCursorStep")
                 .<MyCustomer4, MyCustomer4>chunk(10)
-                .reader(jdbcItemCursorReader())
+//                .reader(jdbcItemCursorReader())
+                .reader(jdbcItemPagingReader(null))
                 .writer(jdbcItemCursorWriter())
                 .build();
     }
@@ -66,5 +72,34 @@ public class JdbcItemConfiguration {
     @Bean
     public ItemWriter jdbcItemCursorWriter() {
         return (items) -> items.forEach(System.out::println);
+    }
+
+    @Bean
+    @StepScope
+    public JdbcPagingItemReader<MyCustomer4> jdbcItemPagingReader(
+            @Value("#{jobParameters['city']}") String city
+    ) throws Exception {
+        Map<String, Object> parameterValues = new HashMap<>();
+        parameterValues.put("city", city);
+        return new JdbcPagingItemReaderBuilder<MyCustomer4>()
+                .name("jdbcItemPagingReader")
+                .dataSource(dataSource)
+                .beanRowMapper(MyCustomer4.class)
+                .queryProvider(pagingQueryProvider())
+                .pageSize(10)
+                .parameterValues(parameterValues)
+                .build();
+    }
+
+    @Bean
+    public PagingQueryProvider pagingQueryProvider() throws Exception {
+        SqlPagingQueryProviderFactoryBean queryProviderFactoryBean = new SqlPagingQueryProviderFactoryBean();
+
+        queryProviderFactoryBean.setDataSource(dataSource);
+        queryProviderFactoryBean.setSelectClause("select *");
+        queryProviderFactoryBean.setFromClause("from tbl_customer");
+        queryProviderFactoryBean.setWhereClause("where city = :city");
+        queryProviderFactoryBean.setSortKey("lastName");
+        return queryProviderFactoryBean.getObject();
     }
 }
