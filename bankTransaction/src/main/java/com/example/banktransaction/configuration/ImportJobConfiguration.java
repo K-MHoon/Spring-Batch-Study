b5df8438c10b8a4b97aae6b2c7f42d1f76eafce3
K-MHOON <azmria117@gmail.com>
2022-04-04 20:39:28 +0900

    feat : 기본 job, step, reader, vo 구성 + application.yml 파일 추가

---
+++ bankTransaction/src/main/java/com/example/banktransaction/configuration/ImportJobConfiguration.java
@@ -0,0 +1,53 @@
+package com.example.banktransaction.configuration;
+
+import com.example.banktransaction.vo.CustomerUpdate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+@RequiredArgsConstructor
+public class ImportJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job importJob() {
+        return jobBuilderFactory.get("importJob")
+                .start(importCustomerUpdates())
+                .build();
+    }
+
+    @Bean
+    public Step importCustomerUpdates() {
+        return stepBuilderFactory.get("importCustomerUpdates")
+                .<CustomerUpdate, CustomerUpdate>chunk(100)
+                .reader(customerUpdateItemReader(null))
+                .processor(customerValidatingItemProcessor(null))
+                .writer(customerUpdateItemWriter())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<CustomerUpdate> customerUpdateItemReader(
+            @Value("#{jobParameters['customerUpdateFile']}") FileSystemResource inputFile) {
+        return new FlatFileItemReaderBuilder<CustomerUpdate>()
+                .name("customerUpdateItemReader")
+                .resource(inputFile)
+                .lineTokenizer(customerUpdatesLineTokenizer())
+                .fieldSetMapper(customerUpdateFieldSetMapper())
+                .build();
+    }
+}
---
+++ bankTransaction/src/main/java/com/example/banktransaction/vo/CustomerUpdate.java
@@ -0,0 +1,9 @@
+package com.example.banktransaction.vo;
+
+public class CustomerUpdate {
+    protected final Long customerId;
+
+    public CustomerUpdate(Long customerId) {
+        this.customerId = customerId;
+    }
+}
